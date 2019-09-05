af6bcb9c553d2de1e4eef2feec478769fabdafaa
Krisztian Holub <kholub1989@gmail.com>
2019-09-04 21:32:34 -0400

    add images and style Navbar Component

--- walbler-client/src/containers/Navbar.js
+++ walbler-client/src/containers/Navbar.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Logo from '../images/warbler-logo.png'
 
 class Navbar extends Component {
   render(){
     return(
       <nav className="navbar navbar-expand">
         <div className="container-fluid">
-          <Link 
-            to="/" 
-            className="navbar-brand"/>
-              <img src="" alt="Warbler Home"/>
-          <Link/>
+          <div className="navbar-header">
+            <Link 
+              to="/" 
+              className="navbar-brand">
+                <img src={Logo} alt="Warbler Home"/>
+            </Link>
+          </div>
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <Link to="/signup">Sing up</Link>
+            </li>
+            <li>
+              <Link to="/signin">Log in</Link>
+            </li>
+          </ul>
         </div>
-        <ul className="nav navbar-nav navbar-right">
-          <li>
-            <Link to="/signup"/>Sing up<Link/>
-          </li>
-          <li>
-            <Link to="/signin"/>Log in<Link/>
-          </li>
-        </ul>
       </nav>
     )
   }
