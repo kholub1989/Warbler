eb7d23af0d828b58ca7dbe0e6ab12224a0546f39
Krisztian Holub <kholub1989@gmail.com>
2019-09-09 19:45:00 -0400

    fix bugs

--- walbler-server/models/message.js
+++ walbler-server/models/message.js
@@ -1,29 +1,37 @@
-const mongoose = require('mongoose');
-const User = require('./user');
+const mongoose = require("mongoose");
+const User = require("./user");
 
-const messageSchema = new mongoose.Schema({
-  text: {
-    type: String,
-    required: true,
-    maxLenght: 160
+const messageSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+      maxLength: 160
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
   },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+  {
+    timestamps: true
   }
-}, {
-  timestamps: true
+);
+
+messageSchema.pre("remove", async function(next) {
+  try {
+    // find a user
+    let user = await User.findById(this.user);
+    // remove the id of the message from their messages list
+    user.messages.remove(this.id);
+    // save that user
+    await user.save();
+    // return next
+    return next();
+  } catch (err) {
+    return next(err);
+  }
 });
 
-messageSchema.pre('remove', async function(next){
-  try {
-    let user = await User.findById(this.user);
-    user.messages.remove(this.id);
-    await user.seve();
-  } catch(err){
-      return next(err)
-  }
-})
-
-const Message = mongoose.model('Message', messageSchema);
+const Message = mongoose.model("Message", messageSchema);
 module.exports = Message;
